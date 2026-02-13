0983bacd46d0c6e328ff316a9a2652523c22d898
fislysandi <dev@fislysandi.xyz>
2026-02-13 16:17:02 +0300

    fix: rename addon id to subtitle_studio to match folder name

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ Addon Configuration
 """
 
 # This is the unique package name of the addon
-# It MUST match the folder name 'subtitle_editor'
-__addon_name__ = "subtitle_editor"
+# It MUST match the folder name 'subtitle_studio'
+__addon_name__ = "subtitle_studio"
